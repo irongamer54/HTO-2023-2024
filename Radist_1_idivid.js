12a27a7a1daae363090f2dcdcd7d4e93142245a7
irongamer54 <61191096+irongamer54@users.noreply.github.com>
2023-11-29 00:38:12 +0700

    Individ Radist 1 - reshena

--- Radist_1_idivid.js
+++ Radist_1_idivid.js
@@ -3,7 +3,22 @@
 
 var receiver
 var transmitter 
+let pre_post_ambale= 126
 
+function crc16(data, offset, length) {
+  if (data == null || offset < 0 || offset > data.length - 1 || offset + length > data.length) {
+      return 0;
+  }
+  
+  crc = 0xFFFF;
+  for (i = 0; i < length; ++i) {
+      crc ^= data[offset + i] << 8;
+      for (j = 0; j < 8; ++j) {
+          crc = (crc & 0x8000) > 0 ? (crc << 1) ^ 0x1021 : crc << 1;
+      }
+  }
+  return crc & 0xFFFF;
+}
 
 function setup() {
   receiver = spacecraft.devices[1].functions[0];
@@ -12,7 +27,14 @@ function setup() {
 
 
 function loop() {
-   let received_packet = receiver.receive(10);
-   transmitter.transmit(received_packet);
+   let received_packet = receiver.receive(262);
+
+   var in_crc = (((received_packet.slice(-3,-2) & 0xff) << 8) | (received_packet.slice(-2,-1) & 0xff));
+
+   var crc = crc16(received_packet.slice(1,-3),0,received_packet.slice(1,-3).length);
+
+   if (in_crc === crc){
+    transmitter.transmit(received_packet);
+   } 
 }
 
